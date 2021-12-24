eb65e46c1dec52f76e4fa0a7f916b069b524e659
Haashim Rehan <haashimr@hotmail.com>
2021-12-23 23:59:41 -0500

    Finished Converting Code
    Now just need to test it

--- Keyboard_code/Keyboard_code.ino
+++ Keyboard_code/Keyboard_code.ino
@@ -4,18 +4,18 @@
 
 BleKeyboard bleKeyboard;
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin = 2;    // the number of the pushbutton pin
-const int ledPin = 4;      // the number of the LED pin
+const int buttonPin = 2; // the number of the pushbutton pin
+const int ledPin = 4;    // the number of the LED pin
 
 // Variables will change:
-int ledState = HIGH;         // the current state of the output pin
-int buttonState;             // the current reading from the input pin
-int lastButtonState = LOW;   // the previous reading from the input pin
+int ledState = HIGH;       // the current state of the output pin
+int buttonState;           // the current reading from the input pin
+int lastButtonState = LOW; // the previous reading from the input pin
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 10;    // the debounce time; increase if the output flickers
+unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
+unsigned long debounceDelay = 10;   // the debounce time; increase if the output flickers
 
 /*//primitives
   int x = 1;
@@ -30,7 +30,8 @@ unsigned long debounceDelay = 10;    // the debounce time; increase if the outpu
 
 */
 
-class Button {
+class Button
+{
     int key;
     int state;
     int lastState = LOW;
@@ -38,37 +39,75 @@ class Button {
   public:
     int pin;
 
-    Button(int _pin, int _key) {
+    Button(int _pin, int _key)
+    {
       pin = _pin;
       key = _key;
     }
+
+    void readSwitch()
+    {
+      int reading = digitalRead(pin);
+      if (reading != lastState)
+      {
+        // reset the debouncing timer
+        lastDebounceTime = millis();
+      }
+
+      if ((millis() - lastDebounceTime) > debounceDelay)
+      {
+        // whatever the reading is at, it's been there for lonHello world
+        if (reading != state)
+        {
+          state = reading;
+
+          // only toggle the LED if the new button state is HIGH
+          if (state == HIGH)
+          {
+            ledState = !ledState;
+            bleKeyboard.write(key);
+            delay(250);
+            bleKeyboard.releaseAll();
+          }
+        }
+      }
+
+      digitalWrite(ledPin, ledState);
+      // save the reading. Next time through the loop, it'll be the lastButtonState:
+      lastState = reading;
+    }
 };
 
-
-class Keypad {
+class Keypad
+{
     int numberOfButtons;
     Button buttons[];
 
   public:
-    Keypad(Button _buttons[]) {
+    Keypad(Button _buttons[])
+    {
       numberOfButtons = sizeof(_buttons) / sizeof(_buttons[0]);
 
-      for (int i = 0; i < numberOfButtons; i++) {
+      for (int i = 0; i < numberOfButtons; i++)
+      {
         buttons[i] = _buttons[i];
       }
     }
 
-    void setInput() {
-      int i = 0;
-
-
-
-      for (int i = 0; i < numberOfButtons; i++) {
+    void setInput()
+    {
+      for (int i = 0; i < numberOfButtons; i++)
+      {
         pinMode(buttons[i].pin, INPUT);
       }
-
     }
 
+    void updateInputs() {
+      for (int i = 0; i < numberOfButtons; i++)
+      {
+        buttons[i].readSwitch();
+      }
+    }
 };
 
 Button minecraft(2, KEY_F1);
@@ -84,65 +123,24 @@ Button youtube(11, KEY_F10);
 
 Button buttonArray[] = {minecraft, inventor, cura, discord, epicgames, github, unity, steam, chrome, youtube};
 
-
 Keypad macropad(buttonArray);
-
 
 //Button inventor(3, KEY_F2);
 
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
   macropad.setInput();
-  //  pinMode(minecraft.pin, INPUT);
-  //pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, ledState);
   bleKeyboard.begin();
 }
 
-
-void loop() {
-  if (bleKeyboard.isConnected()) {
-    readSwitch();
-    //  Serial.println("Sending 'Hello world'...");
+void loop()
+{
+  if (bleKeyboard.isConnected())
+  {
+    macropad.updateInputs();
   }
-
-  //Serial.println("Waiting 5 seconds...");
-  // delay(5000);
-}
-void readSwitch() {
-  // read the state of the switch into a local variable:
-  int reading = digitalRead(buttonPin);
-
-  // check to see if you just pressed the button
-  // (i.e. the input went from LOW to HIGH), and you've waited long enough
-  // since the last press to ignore any noise:Hello world
-
-  // If the switch changed, due to noise or pressing:
-  if (reading != lastButtonState) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }
-
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // whatever the reading is at, it's been there for lonHello world
-    if (reading != buttonState) {
-      buttonState = reading;
-
-      // only toggle the LED if the new button state is HIGH
-      if (buttonState == HIGH) {
-        ledState = !ledState;
-        bleKeyboard.write(KEY_F1);
-        delay(250);
-        bleKeyboard.releaseAll();
-      }
-    }
-  }
-
-  // set the LED:
-  digitalWrite(ledPin, ledState);
-  // save the reading. Next time through the loop, it'll be the lastButtonState:
-  lastButtonState = reading;
 }
